1abd718a7f8aac2f56c0364eeae044ddeddfa7d3
fuzenblue <primrosemprst@gmail.com>
2024-11-29 03:02:21 +0700

    final update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -85,5 +85,28 @@ const userOrders = async (req,res) => {
     }
 }
 
+// Listing order for admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({})
 
-export {placeOrder, verifyOrder, userOrders}
+        res.json({success:true, data:orders})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+// api for updating order status
+const updateStatus = async (req, res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, {status: req.body.status})
+        res.json({success:true, message:"Status Updated"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:"Error"})
+    }
+}
+
+
+export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus}
